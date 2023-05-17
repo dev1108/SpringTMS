1e74949a2b736a2a9d71a5dc596378e9c12b7ddb
unknown <divyanggaursharma@gmail.com>
2023-05-17 06:13:18 -0800

    First

---
+++ tmspro/src/main/java/com/aiminfo/tmspro/TmsproApplication.java
@@ -0,0 +1,13 @@
+package com.aiminfo.tmspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsproApplication.class, args);
+	}
+
+}
